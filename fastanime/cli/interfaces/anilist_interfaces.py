461c3216c0680b1dc9e849cd32de2fce2f298986
Gand0rf <gand0rf@protonmail.com>
2024-11-26 09:19:21 -0500

    More Adjustments

--- fastanime/cli/interfaces/anilist_interfaces.py
+++ fastanime/cli/interfaces/anilist_interfaces.py
@@ -513,7 +513,7 @@ def provider_anime_episode_servers_menu(
     )
 
     # update discord activity for user
-    if config.discord:
+    if config.discord == True:
         discord_proc = multiprocessing.Process(target=discord_updater,args=(provider_anime_title,current_episode_number))
         discord_proc.start()
 
@@ -600,7 +600,7 @@ def provider_anime_episode_servers_menu(
     print("Finished at: ", stop_time)
 
     # stop discord activity updater
-    if config.discord:
+    if config.discord == True:
         discord_proc.terminate()
 
     # update_watch_history
